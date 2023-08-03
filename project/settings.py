c0a78d2f7919b3047665f0538a51769b29113ec9
abed ahmed abdelhadi <83504362+Abed-Ahmed-Abdelhadi@users.noreply.github.com>
2023-08-03 05:43:39 +0100

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -147,8 +147,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Email server configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'abed.ahmed.abdelhadi@gmail.com'
-EMAIL_HOST_PASSWORD = 'lynbcbszzcoeklbm'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
